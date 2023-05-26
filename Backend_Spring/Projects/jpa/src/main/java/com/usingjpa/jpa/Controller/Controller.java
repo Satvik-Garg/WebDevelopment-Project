988208c17b0353c87fa8efa886a4c62d625c2af7
Him7n <knockknockletthedevilin777@proton.me>
2023-05-27 02:34:19 +0530

    Added cros and Fetched the data in the JS

--- Backend_Spring/Projects/jpa/src/main/java/com/usingjpa/jpa/Controller/Controller.java
+++ Backend_Spring/Projects/jpa/src/main/java/com/usingjpa/jpa/Controller/Controller.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 // import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,6 +31,7 @@ public class Controller{
   @Autowired  
   private ProjectServices services;
 
+    @CrossOrigin
     @GetMapping("/getallprojects")
     public List<Project> getAllProjects(){
 
