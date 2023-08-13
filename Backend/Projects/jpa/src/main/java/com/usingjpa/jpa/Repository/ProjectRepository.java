7c00a0c252acce3c9c23e15b447c8c97d6a5514b
Sridhar Suthapalli <sutapalli.sns@gmail.com>
2023-08-13 15:18:15 +0530

    uploaded whole project using react and springboot

--- Backend/Projects/jpa/src/main/java/com/usingjpa/jpa/Repository/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.usingjpa.jpa.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.usingjpa.jpa.Project;
-
-public interface ProjectRepository extends JpaRepository<Project,Integer> {
-    
-}
